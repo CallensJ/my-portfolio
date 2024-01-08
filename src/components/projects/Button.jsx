635b3b47babaea0403087032af871a9f7292cb7d
CallensJ <callens.johan@protonmail.com>
2024-01-08 14:45:46 +0800

    added missing code to filter all categories

--- src/components/projects/Button.jsx
+++ src/components/projects/Button.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 
-const Button = ({filter}) => {
+const Button = ({button, filter}) => {
   return (
     <div className="filters">
-    <button type="button" className="btn" onClick={() => filter('FrontendMentor')}>FrontendMentor</button> 
-    <button type="button" className="btn" onClick={() => filter('Freecodecamp')}>Freecodecamp</button>
-    
+      {/*
+      <button type="button" className="btn" onClick={() => filter('FrontendMentor')}>FrontendMentor</button> 
+      <button type="button" className="btn" onClick={() => filter('Freecodecamp')}>Freecodecamp</button>
+      */}
+      {
+        button.map((cat, i) =>{
+          return <button type="button" className='btn' onClick={()=> filter(cat)}>{cat}</button>
+        })
+      }
     </div>
   )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,12 +4,17 @@ import Card from "./Card";
 import items from "../../Data";
 import Button from "./Button";
 
+const allCategories = ['All', ...new Set(items.map(item => item.category))];
 
 const Projects = () => {
   const [menuItem, setMenuItem] = useState(items);
-  const [buttons, setButtons] = useState([]);
+  const [buttons, setButtons] = useState(allCategories);
 
   const filter = (button) => {
+    if(button === 'All'){
+      setMenuItem(items);
+      return;
+    }
     const filteredData = items.filter(item => item.category === button);
     setMenuItem(filteredData);
   }
@@ -21,7 +26,7 @@ const Projects = () => {
         <h2 className="section__title">Projects</h2>
         <div className="buttons-container">
 
-        <Button filter={filter} />
+        <Button button={buttons} filter={filter} />
         </div>
         <div className="projects__container">
             <Card menuItem={menuItem} />
